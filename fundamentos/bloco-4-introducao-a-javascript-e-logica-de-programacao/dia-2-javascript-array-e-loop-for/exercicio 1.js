b9c39664469d49d555779d6bedf0aab9e4cac0ec
Beterraba <ls6182315@gmail.com>
2022-06-22 20:30:04 -0300

    Desafio 6

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio 1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio 1.js
@@ -1,9 +1,9 @@
-let numbers = [5,9,3,19,70,8,100,120,35,27]
+let numbers = [5,9,3,19,70,8,100,2,35,27]
 
 var maior = 0;
 for (var i = 0; i < numbers.length; i++) {
-   if ( numbers[i] > maior ) {
-      maior = numbers[i];
+   if ( numbers[i] %2 !== 0 ) {
+      maior++
    }
 }
 console.log(maior);
