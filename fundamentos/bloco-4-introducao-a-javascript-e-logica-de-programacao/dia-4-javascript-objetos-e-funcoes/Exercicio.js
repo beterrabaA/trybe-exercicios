054bbaea8102c5242db3d6dc2c00fd0552f465c9
Beterraba <ls6182315@gmail.com>
2022-06-26 21:16:52 -0300

    Exercicio 6

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Exercicio.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Exercicio.js
@@ -1,22 +1,14 @@
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-    recorrente:'Sim'
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: 
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  
 };
 
-let info2 = {
-  personagem: 'Tio Patinhas',
-    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-    nota: 'O último MacPatinhas',
-    recorrente:'Sim'
-}
-
-console.log(info.personagem,'e',info2.personagem);
-console.log(info.origem,'e',info2.origem);
-console.log(info.nota,'e',info2.nota);
-if(info.recorrente === 'Sim' && info2.recorrente === 'Sim') {
-  console.log('Ambos recorrentes')
-} else {
-  console.log('Não recorrentes');
-}
+console.log('O livro favorito de',leitor.nome,leitor.sobrenome,'se chama',leitor.livrosFavoritos.titulo);
