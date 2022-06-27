31be317bdabaca2ea69899885012bbc496c51859
Beterraba <ls6182315@gmail.com>
2022-06-26 22:13:14 -0300

    Exercicio 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Parte II.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Parte II.js
@@ -1,3 +1,4 @@
+function menorQue(){
 const array = [2, 4, 6, 7, 10, 0, -3];
 let maior;
 let menor;
@@ -9,6 +10,7 @@ for(let i = 0;i < array.length;i++) {
     }
 }
 
-console.log(maior)
-console.log(menor);
+return menor
+}
 
+console.log(menorQue());
