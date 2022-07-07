18606a841c837f217283d407bf0b06bf2f936b28
Beterraba <ls6182315@gmail.com>
2022-07-07 10:03:36 -0300

    Exercicio #1

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -1,7 +1,7 @@
 function createDaysOfTheWeek() {
     const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
     const weekDaysList = document.querySelector('.week-days');
-  
+
     for (let index = 0; index < weekDays.length; index += 1) {
       const days = weekDays[index];
       const dayListItem = document.createElement('li');
@@ -9,8 +9,41 @@ function createDaysOfTheWeek() {
   
       weekDaysList.appendChild(dayListItem);
     };
+    
   };
   
   createDaysOfTheWeek();
   
-  // Escreva seu código abaixo.
+  const listDay = document.getElementById('days')
+  let totalDays = document.getElementsByClassName('day')
+
+  const innerDay = listDay.appendChild(document.createElement('li'))
+  innerDay.setAttribute('class','day')
+
+  for (let i = 2; i < 34; i++) {
+  const listDay = document.getElementById('days')
+  const innerDay = listDay.appendChild(document.createElement('li'))
+  innerDay.setAttribute('class','day')
+  innerDay.innerText = i -2
+  totalDays[0].innerText = 29
+  totalDays[1].innerText = 30
+
+  if(innerDay.innerText = i -2 === 24) {
+    innerDay.innerText = i -2
+    innerDay.classList.add('holiday')
+  }else if (innerDay.innerText = i -2 === 25) {
+    innerDay.innerText = i -2
+    innerDay.classList.add('holiday')
+  }else if (innerDay.innerText = i -2 === 31) {
+    innerDay.innerText = i -2
+    innerDay.classList.add('holiday')
+  } else {
+    innerDay.innerText = i -2
+    innerDay.setAttribute('class','day')
+  }
+  }
+
+for (let f = 5; f < totalDays.length; f +=7) {
+  const element = totalDays[f];
+  element.classList.add('friday')
+}
