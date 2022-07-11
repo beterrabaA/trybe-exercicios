5dc20d7414d2d9880121640c42c39bbcda7d4a21
Beterraba <ls6182315@gmail.com>
2022-07-11 19:52:16 -0300

    Exercicio Forms

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,19 @@
+const botaoSubmmit = document.querySelector('.button');
+const inputName = document.querySelector('#userName')
+const inputEmail = document.querySelector('#userEmail')
+const saidaNome = document.getElementById('userInner')
+const saidaEmail = document.getElementById('emailInner')
+const inputCheck1 = document.getElementById('check1')
+const inputCheck2 = document.getElementById('check2')
+const saidaCheck1 = document.getElementById('saidaCheck1')
+const saidaCheck2 = document.getElementById('saidaCheck2')
+
+function saidaTotal(event) {
+    event.preventDefault()
+    saidaNome.innerHTML = inputName.value
+    saidaEmail.innerHTML = inputEmail.value
+    saidaCheck1.innerHTML = inputCheck1.value
+    saidaCheck2.innerHTML = inputCheck2.value
+}
+
+botaoSubmmit.addEventListener('click',saidaTotal)
