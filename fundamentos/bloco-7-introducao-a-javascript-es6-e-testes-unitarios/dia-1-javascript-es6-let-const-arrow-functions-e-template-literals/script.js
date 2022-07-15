99e9c78de16e77b53a7433d9f3c74157120fe5fe
Beterraba <ls6182315@gmail.com>
2022-07-15 19:15:12 -0300

    #2

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -2,7 +2,9 @@ const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 // Seu código aqui.
 const minhaFuncao = (array) => {
-    console.log(array);
+ console.log(array.sort((a,b) => {
+   return a - b
+ }));
 }
 
 minhaFuncao(oddsAndEvens) // será necessário alterar essa linha 😉
