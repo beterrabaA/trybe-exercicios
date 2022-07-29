fa3aa5d992218845a60408fe83aaf409c5ff1641
Beterraba <ls6182315@gmail.com>
2022-07-29 14:03:52 -0300

    Finalização

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/#1 Exercício.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/#1 Exercício.js
@@ -96,8 +96,32 @@ function getNamedBook() {
     return pala
 }
 
+// 4 - Ordene os livros por data de lançamento em ordem decrescente.
 
+function booksOrderedByReleaseYearDesc() {
+    let vra = books.sort((a, b) => a.releaseYear + b.releaseYear)
+    return vra
+}
 
-console.log(authorBornIn1947())
-console.log(smallerName())
-console.log(getNamedBook())
+//   🚀 5 - Faça uma função que retorne true, se todas as pessoas autoras nasceram no século XX, ou false, caso contrário.
+
+function everyoneWasBornOnSecXX() {
+    let re5 = books.every((element) => element.author.birthYear > 1900)
+    return re5
+}
+
+// 🚀 6 - Faça uma função que retorne true, se algum livro foi lançado na década de 80, e false, caso contrário.
+
+function someBookWasReleaseOnThe80s() {
+    let re6 = books.some((element) => element.releaseYear > 1980 && element.releaseYear < 1990)
+    return re6
+}
+
+// 7 - Faça uma função que retorne true, caso nenhum author tenha nascido no mesmo ano, e false, caso contrário.
+
+function authorUnique() {
+    let re7 = books.map((element) => (element.author.birthYear))
+    let filtrado = re7.filter((elem, pos, arr) => arr.indexOf(elem) !== pos)
+    let asd = re7.some((element) => element === (filtrado[0]))
+    return asd
+}
