4544f14b83760f7177b58d8b7ce784d55418d74e
Lerpardo <ls6182315@gmail.com>
2022-09-29 22:52:45 -0300

    set reducers

--- front-end/bloco-15-gerenciamento-do-estado-com-redux/dia-2-usando-o-redux-no-react/exercise-react-with-redux-intro/exercise-three/src/redux/reducers/index.js
+++ front-end/bloco-15-gerenciamento-do-estado-com-redux/dia-2-usando-o-redux-no-react/exercise-react-with-redux-intro/exercise-three/src/redux/reducers/index.js
@@ -1,1 +1,8 @@
 // Neste arquivo você deverá combinar os seus reducers.
+import { combineReducers } from 'redux';
+import carsReducer from './carsReducer';
+import trafficReducer from './trafficReducer';
+
+const rootReducer = combineReducers({ carsReducer, trafficReducer });
+
+export default rootReducer;
