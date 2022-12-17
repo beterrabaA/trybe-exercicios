4b301cf9298fc02217b625036a8309c52b22d4d5
Lerpardo <ls6182315@gmail.com>
2022-12-17 16:54:45 -0300

    6E

--- back-end/bloco-22-introdução-ao-desenvolvimento-web-com-nodeJS/dia-1-nodeJS-runtime-assincrono/addNelsonToFamily.js
+++ back-end/bloco-22-introdução-ao-desenvolvimento-web-com-nodeJS/dia-1-nodeJS-runtime-assincrono/addNelsonToFamily.js
@@ -2,10 +2,10 @@ const fs = require('fs').promises;
 
 async function addNelsonToFamily(nelson) {
     try {
-        const data = await fs.readFile('./simpsons.json','utf8')
+        const data = await fs.readFile('./simpsonsFamily.json','utf8')
         const saturado = JSON.parse(data)
         const novosSimpsons = [...saturado,nelson]
-        await fs.writeFile('./simpsons.json', JSON.stringify(novosSimpsons));
+        await fs.writeFile('./simpsonsFamily.json', JSON.stringify(novosSimpsons));
     } catch (g) {
         console.error(`Erro ao ler o arquivo: ${g.message}`);
     }
